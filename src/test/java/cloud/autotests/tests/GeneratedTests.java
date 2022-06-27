ba203ac518704184be00c2a3fdd8842417f034df
Aleksandr Safronov <asafronov@sberhealth.ru>
2022-06-27 23:13:42 +0300

    picnic

--- src/test/java/cloud/autotests/tests/GeneratedTests.java
+++ src/test/java/cloud/autotests/tests/GeneratedTests.java
@@ -5,8 +5,8 @@ import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.title;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,14 +14,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GeneratedTests extends TestBase {
     @Test
     @Description("Soon to be implemented by you (or QA.GURU engineers)")
-    @DisplayName("Logo \"Wrike\" should exist")
+    @DisplayName("Logo \"Picnic\" should exist")
     void generatedTest() {
-        step("Open https://www.wrike.com/", () -> {
-            step("// todo: just add selenium action");
+        step("Open https://www.picnic.app/", () -> {
+            open("https://www.picnic.app/");
         });
 
         step("check logo", () -> {
-            step("// todo: just add selenium action");
+            $(".logo.transition-trigger").shouldBe(visible);
         });
     }
 
